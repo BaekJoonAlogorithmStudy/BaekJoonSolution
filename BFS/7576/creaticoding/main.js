8b7d5ee5b1dd027244d8e9548368769a42bcfa12
_creco <creaticoding@gmail.com>
2018-11-09 19:24:41 +0900

    feat(not): not finish because of time out

--- BFS/7576/creaticoding/main.js
+++ BFS/7576/creaticoding/main.js
@@ -1,15 +1,20 @@
+// 현재 28% 8초에서 시간초과..
+
 var fs = require('fs');
 var input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 
 var size = input[0].split(" ").reverse();
 var arr = [];
-for(var i=1;i<input.length;i++){
+var inputLength = input.length;
+for(var i=1;i<inputLength;i++){
 	arr[i-1] = input[i].split(" ");	
 }
 
 function initQueue(queue, arr){
-	for(var i=0;i<arr.length;i++){
-		for(var j=0;j<arr[i].length;j++){
+    var size = arr.length;
+	for(var i=0;i<size;i++){
+        var size_2 = arr[i].length;
+		for(var j=0;j<size_2;j++){
 			if(arr[i][j]==1)
 				queue.push([[i,j], 0]);		
 		}
@@ -30,15 +35,11 @@ function check(x, y, arr){
 	}
 	return false;
 }
-
-function queuePush(queue, result, days){
-	if(result != false){
-		queue.push([result, days+1]);
-	}
-}
 function isComplete(arr){
-	for(var i=0;i<arr.length;i++){
-		for(var j=0;j<arr[i].length;j++){
+    var size = arr.length;
+	for(var i=0;i<size;i++){
+        var size_2 = arr[i].length;
+		for(var j=0;j<size_2;j++){
 			if(arr[i][j]==0)
 				return false;
 		}
@@ -51,19 +52,22 @@ queue = initQueue(queue, arr);
 var count = 0;
 
 
-while(queue.length != 0){;
+while(queue.length != 0){
 	var target = queue.shift();
 	count = target[1];
 	var result1 = check(target[0][0]+1, target[0][1],arr);
-	queuePush(queue, result1, target[1]);
+	if(result1 != false)
+        queue.push([result1, count+1]);
 	var result2 = check(target[0][0]-1, target[0][1],arr);
-	queuePush(queue, result2, target[1]);
+	if(result2 != false)
+        queue.push([result2, count+1]);
 	var result3 = check(target[0][0], target[0][1]+1,arr);
-	queuePush(queue, result3, target[1]);
-	var result4 = check(target[0][0], target[0][1]-1,arr);
-	queuePush(queue, result4, target[1]);
+	if(result3 != false)
+        queue.push([result3, count+1]);
+    var result4 = check(target[0][0], target[0][1]-1,arr);
+    if(result4 != false)
+        queue.push([result4, count+1]);
 }
 
 if(!isComplete(arr)) console.log(-1);
 else console.log(count);
-
